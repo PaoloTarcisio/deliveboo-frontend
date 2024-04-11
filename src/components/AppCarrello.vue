4bb88aeedc3f807fbaa006fd00a4e8a29a933c96
Michail Spinelli <michailspinelli96@gmail.com>
2024-04-11 11:54:27 +0200

    aggiunta elementi carrello prototipo

--- src/components/AppCarrello.vue
+++ src/components/AppCarrello.vue
@@ -6,29 +6,29 @@ export default {
 
 data() {
   return {
-  piatti: [{
-      nome: "Carbonara",
-      descrizione: "Pasta con le uova e guanciale, aggiunta pepe nero",
-      prezzo: "$15"
-              },
-              {
-      nome: "Amatriciana",
-      descrizione: "Pasta sugo",
-      prezzo: "£20"
-              },
-              {
-      nome: "Crudo di pesce",
-      descrizione: "Pesce crudo",
-      prezzo: "£25"
-              },
-              {
-      nome: "Tagliata di manzo",
-      descrizione: "Tagliata di manzo",
-      prezzo: "£35"
-              },
-          ]
+        piatti: [{
+            nome: "Carbonara",
+            descrizione: "Pasta con le uova e guanciale, e pepe nero",
+            prezzo: "$15"
+                    },
+                    {
+            nome: "Amatriciana",
+            descrizione: "Pasta sugo",
+            prezzo: "£20"
+                    },
+                ],
+        piattiAggiunti: [{
+
+        
+        }]
       };
     },
+    methods: {
+      addToCart(piatto) {
+        this.piattiAggiunti.push(piatto);
+        console.log("Nuovo carrello:", this.carrello);
+      }
+    }
 };
 </script>
 
@@ -49,10 +49,32 @@ data() {
                                           background-repeat: no-repeat;">   
               <div class="my-5">
                 <div class="container-list-group">
+
                   <ul id="plates-list" class="plates-list">
-                      <button class="add-btn px-3">
-                          Aggiungi +
-                      </button>
+                      <li v-for="(piatto, i) in piatti" :key="i" class="card">
+                          <div class="card-body">
+                              <h5 class="card-title">{{ piatto.nome }}</h5>
+                              <p class="card-text">{{ piatto.descrizione }}</p>
+                              <p class="card-text">{{ piatto.prezzo }}</p>
+                          </div>
+
+                          <button @click="addToCart(piatto)" class="add-btn px-3">
+                            Aggiungi al carrello
+                        </button>
+                      </li>
+
+                      <h3>
+                        Questi sono i piatti aggiunti al carrello
+                      </h3>
+                      <li v-for="(piatto, index) in piattiAggiunti" :key="'added_' + index" class="card">
+                          
+                          <div class="card-body">
+                              <h5 class="card-title">{{ piatto.nome }}</h5>
+                              <p class="card-text">{{ piatto.descrizione }}</p>
+                              <p class="card-text">{{ piatto.prezzo }}</p>
+                          </div>
+                      </li>
+
                   </ul>
                 </div>
               </div>
