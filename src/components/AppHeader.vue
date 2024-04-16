4f5aa02f031d8d8c3d7e3685c6b5cb368dc19475
Michail Spinelli <michailspinelli96@gmail.com>
2024-04-16 20:19:49 +0200

    fix responsive header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -14,30 +14,19 @@ export default {
     <!--NAVBAR-->
     <nav class="navbar navbar-expand-lg fixed-top">
         <div class="container">
-            <a class="navbar-brand me-auto" href="/"><img src="/src/assets/img/deliveboo.png" alt="logo" class="w-75"></a>
+            <span>
+                <a class="navbar-brand me-auto d-none d-lg-inline" href="/"><img src="/src/assets/img/deliveboo.png" alt="logo" class="w-75"></a>
+            </span>
+            <span class="navbar-brand me-auto d-lg-none" >
+                <a href="/"><img src="/src/assets/img/deliveboo-icon.png" alt="logo" class="w-75"></a>
+            </span>
             
-            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                <div class="offcanvas-header">
-                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Logo</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                    
-                </div>
-                <div class="offcanvas-body">
-                    <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
-                        <li class="nav-item">
-                            <a class="nav-link mx-lg-2" href="/">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link mx-lg-2" href="/carrello">Carrello</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <span class="text-secondary me-3">Sei un Ristoratore?</span>
-            <a href="http://127.0.0.1:8000/admin/dashboard" class="login-button">Login</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            <span class="text-secondary me-3">
+                Sei un Ristoratore?
+                <a href="http://127.0.0.1:8000/admin/dashboard" class="login-button">Login</a>
+            </span>
+            
+            
         </div>
     </nav>
     <!--FINE NAVBAR-->
