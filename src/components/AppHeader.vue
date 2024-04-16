28edf1640b882c1c8cbe9511b88d507c8f21fd72
gabrielebottari <gabriele.bottari1@gmail.com>
2024-04-16 22:46:56 +0200

    UPDATE header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -14,23 +14,20 @@ export default {
     <!--NAVBAR-->
     <nav class="navbar navbar-expand-lg">
         <div class="container">
-            <span>
-                <a class="navbar-brand me-auto d-none d-lg-inline" href="/"><img src="/src/assets/img/deliveboo.png" alt="logo" class="w-75"></a>
-            </span>
-            <span class="navbar-brand me-auto d-lg-none" >
-                <a href="/"><img src="/src/assets/img/deliveboo-icon.png" alt="logo" class="w-75"></a>
-            </span>
+            <a class="navbar-brand" href="/">               
+                <img src="/src/assets/img/deliveboo.png" alt="logo" class="d-none d-lg-block w-75">
+                
+                <img src="/src/assets/img/deliveboo-icon.png" alt="logo" class="d-block d-lg-none w-50">
+            </a>
             
             <span class="text-secondary me-3">
                 Sei un Ristoratore?
                 <a href="http://127.0.0.1:8000/admin/dashboard" class="login-button">Login</a>
             </span>
             
-            
         </div>
     </nav>
     <!--FINE NAVBAR-->
-   
 </template>
 
 <style lang="scss" scoped>
